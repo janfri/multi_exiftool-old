4b4ff3f0213d2e22e78dcb45278c20e7b9b73a22
Jan Friedrich <janfri.rubyforge@gmail.com>
2008-05-22 21:31:30 +0200

    Generating of fixtures with extra parameters for exiftool.

--- script/generate_fixture.rb
+++ script/generate_fixture.rb
@@ -1,24 +1,28 @@
 #!/usr/bin/env ruby
 
-unless ARGV.size == 2 
-  $stderr.puts "usage: #$0 [fixturename] [filename]"
-  exit -1
+if ARGV.size < 2 
+  $stderr.puts "usage: #$0 [fixturename] [filename] [exiftool-opts]*"
+  exit(-1)
 end
 
 fixture = ARGV.shift
 filename = ARGV.shift
+opts =  ARGV.map {|a| a.sub(/(.+?=)(.*)/, '\1"\2"')}.join(' ')
 
 fixture_filename = File.join(File.dirname(__FILE__), %w(.. data fixtures), fixture)
 
-cmd = "exiftool -s -t #{filename} > #{fixture_filename}.stdout 2> #{fixture_filename}.stderr"
+cmd = "exiftool -s -t #{opts} #{filename} > #{fixture_filename}.stdout 2> #{fixture_filename}.stderr"
 
-puts cmd if $DEBUG
+if $DEBUG
+  puts cmd
+  exit 0
+end
 
 result = system cmd
 
-if result
+if File.exist?("#{fixture_filename}.stdout") && File.exist?("#{fixture_filename}.stderr")
   puts "Fixture #{fixture} generated."
 else
   puts "Error while generating fixture #{fixture}."
-  exit -1
+  exit(-1)
 end
