315bbe6739939d2f3f90aad75594ad2651b7008f
Jan Friedrich <janfri.rubyforge@gmail.com>
2008-05-29 21:52:30 +0200

    Bugfix: More than one file for generating fixtures.

--- script/generate_fixture.rb
+++ script/generate_fixture.rb
@@ -1,17 +1,16 @@
 #!/usr/bin/env ruby
 
 if ARGV.size < 2 
-  $stderr.puts "usage: #$0 [fixturename] [filename] [exiftool-opts]*"
+  $stderr.puts "usage: #$0 [fixturename] [exiftool-opts]* [filename]+"
   exit(-1)
 end
 
 fixture = ARGV.shift
-filename = ARGV.shift
-opts =  ARGV.map {|a| a.sub(/(.+?=)(.*)/, '\1"\2"')}.join(' ')
+params =  ARGV.map {|a| a.sub(/(.+?=)(.*)/, '\1"\2"')}.join(' ')
 
 fixture_filename = File.join(File.dirname(__FILE__), %w(.. data fixtures), fixture)
 
-cmd = "exiftool -s -t #{opts} #{filename} > #{fixture_filename}.stdout 2> #{fixture_filename}.stderr"
+cmd = "exiftool -s -t #{params} > #{fixture_filename}.stdout 2> #{fixture_filename}.stderr"
 
 if $DEBUG
   puts cmd
