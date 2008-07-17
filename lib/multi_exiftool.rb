4b52022223dbf18de5b1d4bf6555fe9cea440782
Jan Friedrich <janfri.rubyforge@gmail.com>
2008-07-17 17:49:44 +0200

    New Methods MultiExiftool.(read|write).

--- lib/multi_exiftool.rb
+++ lib/multi_exiftool.rb
@@ -2,7 +2,19 @@ require 'multi_exiftool/command_generator'
 require 'multi_exiftool/parser'
 require 'multi_exiftool/read_object'
 require 'multi_exiftool/write_object'
+require 'open3'
 
 module MultiExiftool
+
+  def self.read *filenames
+    cmd = CommandGenerator.read_command_string *filenames
+    stdin, stdout, stderr = Open3.popen3(cmd)
+    result = Parser.parse(stdout, stderr)
+    result.map {|r| ReadObject.new(r.data)}
+  end
+
+
+  def self.write write_object, *filenames
+  end
 
 end
