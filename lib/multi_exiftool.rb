25c01e90884a04793b0fbd41257ea7af2b34888c
Jan Friedrich <janfri.rubyforge@gmail.com>
2008-11-03 22:54:43 +0100

    Refactoring: Saver monkey patching.

--- lib/multi_exiftool.rb
+++ lib/multi_exiftool.rb
@@ -4,28 +4,34 @@ require 'multi_exiftool/read_object'
 require 'open3'
 require 'ostruct'
 
-# Monkey patching :D
+# monkey patching --- the save way :D
 class OpenStruct
-  def to_hash
-    @table
+  unless self.new.respond_to? :to_hash
+    def to_hash
+      @table
+    end
   end
 end
 
 module MultiExiftool
 
-  def self.read *filenames
-    cmd = CommandGenerator.read_command_string *filenames
-    stdin, stdout, stderr = Open3.popen3(cmd)
-    result = Parser.parse(stdout, stderr)
-    result.map {|r| ReadObject.new(r.data)}
-  end
+  class << self
+
+    def read *filenames
+      cmd = CommandGenerator.read_command_string *filenames
+      stdin, stdout, stderr = Open3.popen3(cmd)
+      result = Parser.parse(stdout, stderr)
+      result.map {|r| ReadObject.new(r.data)}
+    end
 
 
-  def self.write change_set, *filenames
-    cmd = CommandGenerator.write_command_string change_set, *filenames
-    stdin, stdout, stderr = Open3.popen3(cmd)
-    result = Parser.parse(stdout, stderr)
-    result.inject(true) {|r| r.errors.empty?}
+    def write change_set, *filenames
+      cmd = CommandGenerator.write_command_string change_set, *filenames
+      stdin, stdout, stderr = Open3.popen3(cmd)
+      result = Parser.parse(stdout, stderr)
+      result.inject(true) {|r| r.errors.empty?}
+    end
+
   end
 
 end
